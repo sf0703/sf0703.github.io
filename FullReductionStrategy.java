befd0bd6ac597f94f5a4ef578f2fcf1c75de27f9
sf0703 <68415188+sf0703@users.noreply.github.com>
2025-06-16 19:01:21 +0800

    Add files via upload

--- FullReductionStrategy.java
+++ FullReductionStrategy.java
@@ -2,14 +2,14 @@ public class FullReductionStrategy implements PricingStrategy {
     @Override
     public double calculatePrice(double appleWeight, double strawberryWeight, double mangoWeight) {
         double originalPrice = appleWeight * new Apple().getPricePerPound() +
-                               strawberryWeight * new Strawberry().getPricePerPound() * 0.8 +
-                               mangoWeight * new Mango().getPricePerPound();
-        if (originalPrice >= 100) {
-            return originalPrice - 10;
-        } else {
-            return originalPrice;
-        }
+                strawberryWeight * new Strawberry().getPricePerPound() * 0.8 +
+                mangoWeight * new Mango().getPricePerPound();
+        // 计算可以减免的次数
+        int discountCount = (int) (originalPrice / 100);
+        // 计算减免的总金额
+        double discountAmount = discountCount * 10;
+        // 返回减免后的价格
+        return originalPrice - discountAmount;
     }
 }
-
 
